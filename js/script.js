429ce78070f91eeb05e48e0e04a712c08f7a9435
Julian3003 <104030626+Julian3003@users.noreply.github.com>
2022-04-25 22:14:42 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function restarTiempo() {
 	}
 }
 setInterval(restarTiempo,1000);
-setInterval(mover,500);
+setInterval(mover,1000);
 //setTimeout(mover('playerAmarillo'),300);
 
 document.getElementById('playerAzul').addEventListener("mouseover",sumarPuntos2)
